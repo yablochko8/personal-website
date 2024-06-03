430fb9afcda30de71207959bfe8e04b10f875c91
yab <hello@factions.ai>
2024-06-03 11:38:34 -0400

    misc

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -7,51 +7,28 @@ export const meta: MetaFunction = () => {
   ];
 };
 
-function Header() {
+function HeaderButton(props){
+  return(
+    <a href={"/" + props.target}>
+      <button className="bg-blue-300 hover:bg-red-900 text-white font-bold py-2 px-4 rounded">
+        {props.title}
+      </button>
+    </a>
+  )
+}
+
+export function Header() {
   return(
     <>
     <h1 className="text-3xl font-bold underline">
     lui's website
   </h1>
+  <HeaderButton target="about" title="about" />
+  <HeaderButton target="projects" title="projects" />
+  <HeaderButton target="games" title="games" />
+  <HeaderButton target="words" title="words" />
 
-  <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-    <ul>
-      <li>
-        <a
-          href="/about"
-          rel="noreferrer"
-        >
-          about me
-        </a>
-      </li>
-      <li>
-        <a
-          href="/projects"
-          rel="noreferrer"
-        >
-          projects
-        </a>
-      </li>
-      <li>
-        <a
-          href="/games"
-          rel="noreferrer"
-        >
-          games
-        </a>
-      </li>
 
-      <li>
-        <a
-          href="/words"
-          rel="noreferrer"
-        >
-          words
-        </a>
-      </li>
-
-    </ul>
-  </div>
   </>
   )
   
